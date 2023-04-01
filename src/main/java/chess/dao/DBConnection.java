269465535f6503ab0bcbbc9072128250ce18c906
JunH <tjdtls690@gmail.com>
2023-04-01 11:35:39 +0900

    :art: refactor : 커넥션 연결에서 예외 발생 시, null을 반환하는 것을 예외 발생으로 개선

--- src/main/java/chess/dao/DBConnection.java
+++ src/main/java/chess/dao/DBConnection.java
@@ -16,9 +16,8 @@ public class DBConnection {
         try {
             return DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USERNAME, PASSWORD);
         } catch (final SQLException e) {
-            System.err.println("DB 연결 오류:" + e.getMessage());
             e.printStackTrace();
-            return null;
+            throw new IllegalArgumentException("DB 연결 오류:" + e.getMessage());
         }
     }
 }
